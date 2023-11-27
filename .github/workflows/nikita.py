ff2a75fde1fe79a69a0b8efa3b1587cbd2505b94
Nikita Maslennikov <n.maslennikov@hh.ru>
2023-11-27 17:26:30 +0300

    1

---
+++ .github/workflows/nikita.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+import os
+
+input_variable = os.environ['INPUT_STORE']
+
+
+# with open("CODEOWNERS") as f:
+#     data = f.read()
+
+
+# owners = CodeOwners(data)
+
+# print(owners.of("p.js"))
+
+
+print("Input Variable:", input_variable)
